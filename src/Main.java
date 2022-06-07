ec91bb5085825ef5d740ef8905c8bb08de158700
Sannava <deon3006@mail.ru>
2022-06-07 19:42:41 +0300

    add sort methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
-        Test test = new Test();
-        test.passTest();
+//        Test test = new Test();
+//        test.passTest();
+        Sort sort = new Sort();
+        Sort sort2 = new Sort();
 
+        Date start = new Date();
+        sort.add();
+        Date end = new Date();
+
+        Date start2 = new Date();
+        sort2.addBubble();
+        Date end2 = new Date();
+
+        System.out.println("Время сортировки методом sort: " + (end.getTime() - start.getTime()) +
+        " Время сортировки пузырьком: " + (end2.getTime() - start2.getTime()));
     }
 }
